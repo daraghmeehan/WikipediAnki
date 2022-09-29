6992347f419c4c4e1a6d0fa471c7ab431c006c15
daraghmeehan <daraghmeehan@gmail.com>
2022-09-29 18:47:31 +0200

    Reviewed and approved version 1

--- wikipedianki.py
+++ wikipedianki.py
@@ -12,41 +12,26 @@ nlp = spacy.load("en_core_web_sm")  # loading the language model
 
 
 def run_wikipedianki() -> None:
+    """Runs the WikipediAnki program."""
 
     print("\nWelcome to WikipediAnki!")
 
     flashcards = pd.DataFrame(columns=["Question", "Answer", "Hint", "Title", "URL"])
 
-    still_studying = True
-
     try:
-        while still_studying:
+        while True:
 
             # choose page
             wikipedia_url, soup = choose_page()
             if wikipedia_url == "b":
                 # exit condition
-                still_studying = False
-                continue
+                break
 
-            page_title = soup.find(id="firstHeading").text
-            print(f'You have chosen the page "{page_title}"')
+            new_cards = create_new_cards(wikipedia_url, soup)
 
-            # make cards
-            new_cards = study_page(soup)
-
-            # add extra page information to each card
-            for card in new_cards:
-                card["Title"] = page_title
-                card["URL"] = wikipedia_url
-
-            # add new cards to our collection
-            new_cards = pd.DataFrame(new_cards)
             flashcards = pd.concat([flashcards, new_cards], ignore_index=True)
 
-            # change page or exit
-            print("\nWould you like to choose another page?")
-            still_studying = confirm_with_user()
+            print("\nYour new cards have been saved.")
 
     except:
         print("Error encountered. Saving current flashcards and exiting...")
@@ -56,8 +41,8 @@ def run_wikipedianki() -> None:
     save_flashcards_to_csv(flashcards)
 
 
-# ask the user what Wikipedia page to learn from, and return the final URL and BeautifulSoup's soup object of the page
 def choose_page() -> tuple[str, BeautifulSoup]:
+    """Asks the user what Wikipedia page to learn from, and returns the final URL and BeautifulSoup's soup object of the page."""
 
     wikipedia_url = input(
         '\nWhat Wikipedia page do you want to learn from? Or input "b" to quit and save flashcards.\n'
@@ -82,8 +67,27 @@ def choose_page() -> tuple[str, BeautifulSoup]:
     return wikipedia_url, soup
 
 
-# using a given page's soup object, create new flashcards of user-selected sentences, returning a list of the new cards to add to our deck
-def study_page(soup: BeautifulSoup) -> list[dict]:
+def create_new_cards(wikipedia_url: str, soup: BeautifulSoup) -> pd.DataFrame:
+    """The user creates new cards by studying the given Wikipedia page."""
+
+    page_title = soup.find(id="firstHeading").text
+    print(f'You have chosen the page "{page_title}"')
+
+    # make cards
+    new_cards = study_page(soup)
+
+    # add extra page information to each card
+    for card in new_cards:
+        card["Title"] = page_title
+        card["URL"] = wikipedia_url
+
+    # add new cards to our collection
+    new_cards = pd.DataFrame(new_cards)
+    return new_cards
+
+
+def study_page(soup: BeautifulSoup) -> list[dict[str, str]]:
+    """Using a given page's soup object, creates new flashcards of user-selected sentences, and returns a list of the new cards to add to our deck."""
 
     page_title = soup.find(
         id="firstHeading"
@@ -106,16 +110,15 @@ def study_page(soup: BeautifulSoup) -> list[dict]:
             return new_cards
 
         if next_card == {}:
-            still_creating_cards = False
-            continue
+            return new_cards
 
         new_cards.append(next_card)
 
     return new_cards
 
 
-# retrieve the text of the given page, with basic preprocessing
 def retrieve_text(soup: BeautifulSoup) -> spacy.tokens.doc.Doc:
+    """Retrieves the text of the given page, with basic preprocessing."""
 
     all_paragraphs = soup.find_all("p")[1:-1]  # first and last paragraphs are "\n"
     text = "\n".join([paragraph.text.strip() for paragraph in all_paragraphs])
@@ -124,19 +127,18 @@ def retrieve_text(soup: BeautifulSoup) -> spacy.tokens.doc.Doc:
     return text
 
 
-# given sentences of Wikipedia page, create new card from a user-selected sentence
-def create_next_card(sents: list[spacy.tokens.span.Span], page_title: str) -> dict:
-
-    next_card = {}
+def create_next_card(
+    sents: list[spacy.tokens.span.Span], page_title: str
+) -> dict[str, str]:
+    """Given sentences of Wikipedia page, creates new card from a user-selected sentence."""
 
     sentence_substring = input(
         '\nType a substring of the sentence you want to make a flashcard for.\nEnter "b" to cancel.\n'
     )
 
     if sentence_substring == "b":
-        print("Are you sure you want to stop making new cards?")
-        if confirm_with_user():
-            return next_card
+        if confirm_with_user("Are you sure you want to stop making new cards?"):
+            return {}
         else:
             return create_next_card(sents, page_title)
 
@@ -145,7 +147,7 @@ def create_next_card(sents: list[spacy.tokens.span.Span], page_title: str) -> di
 
     if len(matching_sents) == 0:
         print("No matching sentences. Please try again.")
-        return create_next_card(sents)
+        return create_next_card(sents, page_title)
 
     if len(matching_sents) > 5:
         print(
@@ -167,8 +169,8 @@ def create_next_card(sents: list[spacy.tokens.span.Span], page_title: str) -> di
     return next_card
 
 
-# ask user which line of those that match the user's input they want to make a flashcard from
 def choose_line(sents: list[spacy.tokens.span.Span]) -> int:
+    """Asks user which line they want to make a flashcard from of those that match the user's input."""
 
     print('\nChoose which line to learn from, or go back with "b".')
 
@@ -184,17 +186,17 @@ def choose_line(sents: list[spacy.tokens.span.Span]) -> int:
     else:
         line_number_to_learn = int(line_number_to_learn)
 
-    if line_number_to_learn not in list(range(1, len(sents) + 1)):
+    if 1 <= line_number_to_learn <= len(sents):
+        return line_number_to_learn
+    else:
         print("Please try again and choose a valid line.")
         return choose_line(sents)
 
-    return line_number_to_learn
 
-
-# the user determines what the card for the given line will look like, and the completed card is returned
 def create_card_from_line(
     line_to_learn: spacy.tokens.span.Span, page_title: str
-) -> dict:
+) -> dict[str, str]:
+    """The user determines what the card for the given line will look like, and the completed card is returned."""
 
     print("\nPlease choose from the words of the sentence.")
     # printing words of line
@@ -206,8 +208,7 @@ def create_card_from_line(
     )
 
     if selected_spans_str == "b":
-        print("Still want to make a flashcard for this sentence?")
-        if confirm_with_user():
+        if confirm_with_user("Still want to make a flashcard for this sentence?"):
             return create_card_from_line(line_to_learn, page_title)
         else:
             return {}
@@ -217,87 +218,32 @@ def create_card_from_line(
         question = line_to_learn.text
         answer = page_title
 
-    else:
-        # otherwise need to parse the given spans and build the question and answers to print on the flashcard
-        all_spans = []  # to track all the spans mentioned
-        span_numbers = (
-            {}
-        )  # to track, for a given index, which span group (i.e. the answer) it corresponds to
-        answers = []  # to collect the answers for the given sub_spans
-
-        try:
-            selected_spans = ast.literal_eval(selected_spans_str)
-            assert isinstance(selected_spans, list)
-
-            if isinstance(selected_spans[0], int):
-                # if only have one span, e.g. "[1,2,3]" or "[5]"
-                selected_spans = [
-                    selected_spans
-                ]  # can trivially treat as a nested span
-
-            elif not isinstance(selected_spans[0], list):
-                raise
-
-            # now have a list of spans "[[1,2], [5,6]]", or simply "[[1,2,3]]"
-            all_spans = [
-                idx for sub_span in selected_spans for idx in sub_span
-            ]  # simply extracting every word index from our list of lists
-            for i, sub_span in enumerate(selected_spans, start=1):
-                # now building answer for given sub_span, and assigning span numbers (corresponding answer) for each index of the sub-span
-                answer = ""
-
-                for j, idx in enumerate(sub_span, start=1):
-                    span_numbers[
-                        idx
-                    ] = i  # assigns indiviual index to a specific span (answer), to use when creating the flashcard
-
-                    if j != len(sub_span):
-                        answer += line_to_learn[idx - 1].text_with_ws
-                    else:
-                        # don't add whitespace if at the last sub_span index (don't want an answer with a space at the end)
-                        answer += line_to_learn[idx - 1].text
-
-                answers.append(answer)
-
-            question = ""
-            for i in range(1, len(line_to_learn) + 1):
-                token = line_to_learn[i - 1]
-                if i in all_spans:
-                    span_number = span_numbers[i]  # like a reverse dict search
-                    question += f"[{span_number}]"
-                    question += token.whitespace_
-                else:
-                    question += token.text_with_ws
-
-            answer = ""
-            for i, individual_answer in enumerate(answers, start=1):
-                if i == len(answers):
-                    answer += f"{i} {individual_answer}"
-                else:
-                    answer += f"{i} {individual_answer}\n"
-
-        except:
-            print("Please try again with a valid span.")
-            return create_card_from_line(line_to_learn, page_title)
-
-    # cleaning up whitespace inconsistencies with bs4 and spacy
-    question = question.strip()
-    answer = answer.strip()
+    # otherwise need to parse the given spans and build the question and answers to print on the flashcard
+    try:
+        selected_spans = parse_selected_subspans(selected_spans_str)
+        question, all_answer_strings = extract_answers_from_line_to_learn(
+            line_to_learn, selected_spans
+        )
+        answer = build_answer(all_answer_strings)
+    except:
+        print(
+            "Cannot create a card with chosen spans. Please try again with a valid span selection."
+        )
+        return create_card_from_line(line_to_learn, page_title)
 
     print_card_preview(question, answer)
 
     hint = ""
 
-    print("\nDo you want to add a hint?")
-    if confirm_with_user():
+    if confirm_with_user("\nDo you want to add a hint?"):
         hint = write_hint()
-        hint = hint.replace("\\n", "\n")
+        hint = hint.replace("\\n", "\n")  # to handle multiple line hints
         print_card_preview(question, answer, hint)
 
-    print("\nFinally, would you like to edit any field?")
-    if confirm_with_user():
+    if confirm_with_user("\nFinally, would you like to edit any field?"):
         question, answer, hint = edit_fields(question, answer, hint)
-        print_card_preview(question, answer)
+    else:
+        print_card_preview(question, answer, hint)
 
     card = {"Question": question, "Answer": answer, "Hint": hint}
 
@@ -306,16 +252,106 @@ def create_card_from_line(
     return card
 
 
-# the user writes what they would like their hint to be
+def parse_selected_subspans(selected_spans_str: str) -> list[list[int]]:
+    """Parses the user's inputted subspan selection, raising an error if not in the format expected."""
+
+    selected_spans = ast.literal_eval(selected_spans_str)
+    assert isinstance(selected_spans, list)
+
+    if isinstance(selected_spans[0], int):
+        # if only have one span, e.g. "[1,2,3]" or "[5]"
+        selected_spans = [selected_spans]  # can trivially treat as a nested span
+
+    elif not isinstance(selected_spans[0], list):
+        raise
+
+    return selected_spans
+
+
+def extract_answers_from_line_to_learn(
+    line_to_learn: spacy.tokens.span.Span, selected_spans: list[list[int]]
+) -> tuple[str, list[str]]:
+    """Extracts the answer substrings from our chosen line, returning the formatted question string and a list of the corresponding answers that were removed."""
+
+    all_answer_positions = (
+        []
+    )  # to track all the token indices in the sentence to be removed
+    span_numbers = (
+        {}
+    )  # to track, for a given index, which span group (i.e. the answer) it corresponds to
+    answer_strings = []  # to collect the answers for the given subspans
+
+    # reminder that selected_spans is a list of spans, e.g. [[1,2], [5,6]], or simply [[1,2,3]]
+    all_answer_positions = [
+        idx for sub_span in selected_spans for idx in sub_span
+    ]  # simply extracting every index from our list of lists (which will tell us quickly if a given position is part of an answer or not)
+
+    for sub_span_number, sub_span in enumerate(selected_spans, start=1):
+        answer_string = ""
+
+        for position_in_line in sub_span:
+            span_numbers[
+                position_in_line
+            ] = sub_span_number  # assigns individual index in our line to a specific subspan (answer), to use when creating the flashcard
+
+            # collecting parts of answer string for given subspan
+            answer_string += line_to_learn[position_in_line - 1].text_with_ws
+
+        answer_string = (
+            answer_string.strip()
+        )  # to remove trailing whitespace when we collected the parts of the string
+        answer_strings.append(answer_string)
+
+    question = build_question(line_to_learn, all_answer_positions, span_numbers)
+
+    return question, answer_strings
+
+
+def build_question(
+    line_to_learn: spacy.tokens.span.Span,
+    all_answer_positions: list[int],
+    span_numbers: dict[int, int],
+) -> str:
+    """Builds the question side of the flashcard for our chosen line, replacing all token positions that form the question with their respective subspan number."""
+
+    question = ""
+
+    for i in range(1, len(line_to_learn) + 1):
+        token = line_to_learn[i - 1]
+        if i in all_answer_positions:
+            span_number = span_numbers[i]  # like a reverse dict search
+            question += f"[{span_number}]"
+            question += token.whitespace_
+        else:
+            question += token.text_with_ws
+
+    # cleaning up potential whitespace inconsistencies with bs4 and spacy
+    question = question.strip()
+    return question
+
+
+def build_answer(answers: list[str]) -> str:
+    """Builds the answer side of the flashcard from a list of the individual subspan answers."""
+
+    answer = "\n".join(
+        f"{i} - {individual_answer}"
+        for i, individual_answer in enumerate(answers, start=1)
+    )
+
+    # cleaning up potential whitespace inconsistencies with bs4 and spacy
+    answer = answer.strip()
+    return answer
+
+
 def write_hint() -> str:
+    """The user writes what they would like their hint to be."""
 
     hint = input(
         '\nPlease type your hint. Use "\\n" to create multiple line hints for multiple spans.\nEnter "b" to cancel.\n'
     )
 
     if hint in ["", "b"]:
-        print("\nWant to skip making a hint?")
-        if confirm_with_user():
+        if confirm_with_user("\nWant to skip making a hint?"):
             return ""
         else:
             return write_hint()
@@ -323,8 +359,8 @@ def write_hint() -> str:
         return hint
 
 
-# gives the user a preview of what their card will look like, so they can choose whether or not to make further changes
 def print_card_preview(question: str, answer: str, hint="") -> None:
+    """Gives the user a preview of what their card will look like, so they can choose whether or not to make further changes."""
 
     print("\nThis is what your flashcard will look like:\n")
     print(f"Question:\n{question}")
@@ -334,8 +370,8 @@ def print_card_preview(question: str, answer: str, hint="") -> None:
     print(f"Answer:\n{answer}")
 
 
-# allows the user to edit the fields of the flashcard in case they aren't happy
-def edit_fields(question: str, answer: str, hint: str) -> tuple([str, str, str]):
+def edit_fields(question: str, answer: str, hint: str) -> tuple[str, str, str]:
+    """Allows the user to edit the fields of the flashcard in case they aren't happy."""
 
     field = input(
         '\nWhich field would you like to edit?\n"q" for question, "a" for answer, "h" for hint, "b" to cancel.\n'
@@ -376,15 +412,18 @@ def edit_fields(question: str, answer: str, hint: str) -> tuple([str, str, str])
         else:
             hint = new_hint
 
-    print("Finished editing?")
-    if confirm_with_user():
+    print_card_preview(question, answer, hint)
+
+    if confirm_with_user("Finished editing?"):
         return question, answer, hint
     else:
         return edit_fields(question, answer, hint)
 
 
-# simple function to confirm choices with the user
-def confirm_with_user() -> bool:
+def confirm_with_user(prompt="") -> bool:
+    """Confirms choices with the user using the given prompt."""
+
+    print(prompt)
 
     y_or_n = input("[y/n] ")
     if y_or_n == "y":
@@ -395,8 +434,8 @@ def confirm_with_user() -> bool:
         return confirm_with_user()
 
 
-# saves all the flashcards we have created to a csv, which allows easy importing into Anki
 def save_flashcards_to_csv(flashcards: pd.core.frame.DataFrame) -> None:
+    """Saves all the flashcards we have created to a csv, which allows easy importing into Anki."""
 
     number_of_cards = len(flashcards.index)
     if number_of_cards == 0:
